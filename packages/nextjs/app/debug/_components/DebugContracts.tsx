53c55c47c27af9ed2237b5f0ceb898c7e78ab5d9
dhruvrohit229 <rohitdhruv205@gmail.com>
2025-08-13 18:56:55 +0000

    initialized multisig wallet challenge

--- packages/nextjs/app/debug/_components/DebugContracts.tsx
+++ packages/nextjs/app/debug/_components/DebugContracts.tsx
@@ -32,7 +32,7 @@ export default function DebugContracts() {
     try {
       if (typeof window === "undefined") return;
 
-      const contractAddress = "0x85bd1a76e588e2539fd450afd6255806e7026049";
+      const contractAddress="0xece258e0586cfe00fd9a455b580819ca64eab9cf";
       const rpcUrl = process.env.NEXT_PUBLIC_RPC_URL || '';
       const privateKey = process.env.NEXT_PUBLIC_PRIVATE_KEY || '';
       // const contractAddress = process.env.NEXT_PUBLIC_CONTRACT_ADDRESS;
